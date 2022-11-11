5d5cc7e2b91c8b82ca6b4af54d8fce7b93a060ee
Hans Castro <hanscastroadmn@gmail.com>
2022-11-12 00:12:08 +0100

    [TESTS] Added first test in symfony

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ up:
 	docker compose up -d
 down:
 	docker compose down
+shell:
+	docker compose run php-fpm bash
+unit-tests:
+	docker compose run php-fpm php bin/phpunit tests/unit/
---
+++ src/tests/unit/HelloWorldTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class HelloWorldTest extends KernelTestCase
+{
+    public function testHello():void
+    {
+        $this->assertTrue(true);
+    }
+}
