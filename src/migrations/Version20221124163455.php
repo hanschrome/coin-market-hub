b269b66a479bdcff2cbc8f7e483206df78686d45
Hans Castro <hanscastroadmn@gmail.com>
2022-12-15 06:53:01 +0100

    [Prices] Working price saving

--- src/migrations/Version20221124163455.php
+++ src/migrations/Version20221124163455.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20221124163455 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,13 +16,11 @@ final class Version20221124163455 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE prices (id int NOT NULL AUTO_INCREMENT, uuid VARCHAR(255) NOT NULL, provider_uuid VARCHAR(255) NOT NULL, raw_prices TEXT(100000) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE prices');
     }
 }
