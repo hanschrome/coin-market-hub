f4ea43ca2f188110098f57f0553e86a61a75c5a6
Hans Castro <hanscastroadmn@gmail.com>
2022-12-21 07:55:37 +0100

    [Prices] Implemented (file)cache system for quick response from last minute

--- src/src/Controller/PricesController.php
+++ src/src/Controller/PricesController.php
@@ -1,34 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Repository\DoctrinePricesRepository;
 use CryptoMarketPrices\Aplication\Prices\GetPricesAction;
-use CryptoMarketPrices\Domain\Prices\IPrices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class PricesController extends AbstractController
 {
     private DoctrinePricesRepository $doctrinePricesRepository;
+    private FilesystemAdapter $filesystemAdapter;
 
-    public function __construct(DoctrinePricesRepository $doctrinePricesRepository)
-    {
+    public function __construct(DoctrinePricesRepository $doctrinePricesRepository) {
         $this->doctrinePricesRepository = $doctrinePricesRepository;
+        $this->filesystemAdapter = new FilesystemAdapter();
     }
 
     #[Route('/api/v1/prices')]
     public function get(): Response
     {
-        $getPricesAction = new GetPricesAction($this->doctrinePricesRepository);
+        return new JsonResponse(['binance' =>
+            $this->filesystemAdapter->get('api_v1_prices_get', function (ItemInterface $item) {
+                $item->expiresAfter(60);
 
-        /** @var IPrices $prices */
-        $prices = $getPricesAction();
+                $getPricesAction = new GetPricesAction($this->doctrinePricesRepository);
 
-        return new JsonResponse([
-            'binance' => json_decode($prices->getRawPrices())
+                return json_decode($getPricesAction()->getRawPrices());
+            })
         ]);
     }
 }
